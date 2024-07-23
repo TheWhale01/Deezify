be127b30f67e847f336a2c2a90b1e9bc51009431
TheWhale01 <ard.rasp01@gmail.com>
2024-07-23 19:00:46 +0200

    working on Party creation

--- frontend/src/routes/(app)/+layout.svelte
+++ frontend/src/routes/(app)/+layout.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
 	async function logout(): Promise<void> {
-		const response = await fetch('');
-		if (response.status !== 200)
-			return;
+		const response = await fetch("");
+		if (response.status !== 200) return;
 		// Remove cookie
 		// redirect to login
 	}
 </script>
+
+<svelte:head>
+	<script src="https://sdk.scdn.co/spotify-player.js"></script>
+</svelte:head>
 
 <button on:click={logout}>Log Out</button>
 <slot></slot>
