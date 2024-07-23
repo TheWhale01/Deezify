9a46da931eb5f8496bad31ada47cf4f1d58ec8d7
TheWhal01 <ard.rasp01@gmail.com>
2024-07-23 16:17:18 +0200

    added scopes for Spotify playback

---
+++ frontend/src/routes/(app)/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	async function logout(): Promise<void> {
+		const response = await fetch('');
+		if (response.status !== 200)
+			return;
+		// Remove cookie
+		// redirect to login
+	}
+</script>
+
+<button on:click={logout}>Log Out</button>
+<slot></slot>
---
+++ frontend/src/routes/(app)/join/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	let party_id: string = '';	
+	
+	async function join_party(): Promise<void> {
+		console.log(party_id);
+		console.log("Joinning party");
+	}
+</script>
+
+<h2>Join Party</h2>
+
+<p>Scan QR code</p>
+<button>Scan !</button>
+<p>or</p>
+<form method="POST" on:submit|preventDefault={join_party}>
+	<label for="party code">Enter party code</label>
+	<input type="text" name="party code" bind:value={party_id} />
+	<button type="submit">Join !</button>
+</form>
